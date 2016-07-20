5d88b076e560181a650673c4ab0c5d7dbbac963a
Tyjow <platon.jonathan@gmail.com>
2016-07-20 17:17:24 +0200

    debut pomodoro

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-/* * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * *
- * * YOUR CODE GOES HERE ! * *
- * * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * */
+function doing() {
+	var field = $("#task").val();
+	$("h2:first").text("Doing " + field);
+}
+
+var minutes = 0;
+var seconds = 0;
+var timerInterval = null;
+
+function timer() {
+
+	minutes++;
+    seconds++;
+        
+	$('#time').text(minutes + ":" + seconds);
+
+}
+
+$("#play").click(function(){
+	doing();
+	timer();
+	timerInterval = setInterval(timer, 1000);
+});
+
+$("#stop").click(function(){
+	var field = $("#task").val('');
+	$("h2:first").text("Doing");
+});
